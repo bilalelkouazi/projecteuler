c6a3bb13070d8b03de624df867f8b7568eb0a0b0
bilalelkouazi <b.kouazi@gmail.com>
2019-01-04 15:07:42 +0100

    Adding tabs and new lines

--- euler0003.c
+++ euler0003.c
@@ -6,11 +6,12 @@
 int main() {
 	double num=600851475143;
 	int i;
+	printf("Finding divisible number for %.0f\n\n",num);
 	for(i=2;i<=num;i++) {
 		if(fmod(num, i)==0) {
 			printf("%d",i);
 			num=num/i;
-			printf("---->%f\n",num);
+			printf("=\t\t%.0f\n",num);
 			i=2;
 		}
 	}
